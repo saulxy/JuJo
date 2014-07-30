781157c11afc5ea7bc28cfde2eae0a2fad175b76
sauljetsmi <sauljetsmi@gmail.com>
2014-07-29 22:42:24 -0500

    *Movimiento del personaje

--- JuJo/public_html/js/behavior.js
+++ JuJo/public_html/js/behavior.js
@@ -4,20 +4,34 @@
  * and open the template in the editor.
  */
 var posX = 0;
+var posY = 0;
 function load() { 
  var personaje = document.getElementById("personaje"); 
  //personaje.addEventListener("keydown", function() { alert("alert");});
  
- window.addEventListener('keydown', function (e) {
-    if (e.keyCode === 38) {
-    alert("arriba");
-    }
-    if (e.keyCode === 39) {
-    posX += 40;
-    personaje.style["left"] = posX + "px";
-    }
-}, false);
+ window.addEventListener('keydown', 
  
-}
+            function(e) {
+                if (e.keyCode === 39) {
+                    posX += 40;
+                    personaje.style["left"] = posX + "px";
+                }
+                if (e.keyCode === 37) {
+                    posX -= 40;
+                    personaje.style["left"] = posX + "px";
+                }
+                if (e.keyCode === 40) {
+                    posY += 40;
+                    personaje.style["top"] = posY + "px";
+                }
+                if (e.keyCode === 38) {
+                    posY -= 40;
+                    personaje.style["top"] = posY + "px";
+                }
+            }
+);
  
+    }
+
+
 document.addEventListener("DOMContentLoaded",load);
